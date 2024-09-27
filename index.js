396496f6db23d42dc12e293aad7252acad73b325
zar-690 <85935320+zar-690@users.noreply.github.com>
2024-09-27 18:33:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+// Sample JSON Data
+const chemicalData = [
+  { id: 1, chemicalName: 'Chemical A', vender: 'Vendor 1', density: 1.5, viscosity: 2.1, packaging: 'Bottle', packSize: 5, unit: 'kg', quantity: 100 },
+  { id: 2, chemicalName: 'Chemical B', vender: 'Vendor 2', density: 1.2, viscosity: 1.8, packaging: 'Can', packSize: 10, unit: 'kg', quantity: 150 },
+  { id: 3, chemicalName: 'Chemical C', vender: 'Vendor 3', density: 1.7, viscosity: 2.5, packaging: 'Drum', packSize: 50, unit: 'L', quantity: 200 },
+  { id: 4, chemicalName: 'Chemical D', vender: 'Vendor 4', density: 1.4, viscosity: 2.0, packaging: 'Pouch', packSize: 2, unit: 'L', quantity: 300 },
+  { id: 5, chemicalName: 'Chemical E', vender: 'Vendor 5', density: 1.9, viscosity: 2.7, packaging: 'Bottle', packSize: 10, unit: 'kg', quantity: 50 },
+  { id: 6, chemicalName: 'Chemical F', vender: 'Vendor 6', density: 1.1, viscosity: 1.5, packaging: 'Can', packSize: 25, unit: 'kg', quantity:80}
+]
